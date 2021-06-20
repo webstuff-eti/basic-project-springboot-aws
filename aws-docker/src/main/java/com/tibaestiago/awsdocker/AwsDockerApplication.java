ec88ae5b267967c40d5990526dbffaf4cd98d7b6
webstuff-eti <tibaestiago@gmail.com>
2021-06-20 17:45:39 -0300

    criando projeto

---
+++ aws-docker/src/main/java/com/tibaestiago/awsdocker/AwsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.tibaestiago.awsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDockerApplication.class, args);
+	}
+
+}
---
+++ aws-docker/src/test/java/com/tibaestiago/awsdocker/AwsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibaestiago.awsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
