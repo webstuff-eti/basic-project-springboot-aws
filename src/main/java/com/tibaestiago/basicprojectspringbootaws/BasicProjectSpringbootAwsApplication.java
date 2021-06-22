ce4ea31ee6b5f882c0a28c3abd96a36b05c6873a
webstuff-eti <tibaestiago@gmail.com>
2021-06-22 11:59:33 -0300

    resolvendo conflitos

---
+++ src/main/java/com/tibaestiago/basicprojectspringbootaws/BasicProjectSpringbootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.tibaestiago.basicprojectspringbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectSpringbootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProjectSpringbootAwsApplication.class, args);
+	}
+
+}
